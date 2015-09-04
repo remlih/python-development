0546779c91b2cb46de902344b4eb89d1436a443d
Roberto Cervantes <rcervantes@outlook.com>
2015-09-03 20:10:32 -0700

    Create call_rest.py

---
+++ call_rest.py
@@ -0,0 +1,17 @@
+#import the modules
+import requests
+import json
+
+url = 'http://localhost/Get'
+params = {"user": "roberto.cervantes@test.com", "password": ".sample", "token": "", "hash": "", "device": 1, "newPassword": "" }
+headers = { "Content-Type": "Application/json", "X-Forwarded-For": "127.0.0.1", "GBMDigitalIdentityApp" : 1 }
+
+def call(url, params, headers):
+    print('calling: ' + url)
+    try:    
+        response = requests.post(url, data = json.dumps(params), headers=headers)
+        print(response.json())
+    except Exception as ex:
+        print ('Unexpected error: ', ex)
+
+call(url, params, headers)
